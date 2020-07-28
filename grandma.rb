136fd6af7a2030643acab576e36e61821485a9f5
Kiersty Shoultz <kierstycaekz@live.com>
2020-07-28 17:01:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,13 @@
 def speak_to_grandma(phrase)
   if (phrase=="I LOVE YOU GRANDMA!" )
     "I LOVE YOU TOO PUMPKIN!"
-  end 
+    elsif (phrase=="#{phrase}".upcase)
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+ end
+ 
+ end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
